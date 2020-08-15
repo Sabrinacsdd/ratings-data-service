67a3bce957029b16ed2c6ceab281eb48924e3a94
s <Sabrinacsdd@users.noreply.github.com>
2020-08-14 22:30:57 -0700

    08.14.20

---
+++ src/main/java/com/microservice/ratingsdataservice/models/Rating.java
@@ -0,0 +1,4 @@
+package com.microservice.ratingsdataservice.models;
+
+public class Rating {
+}
---
+++ src/main/java/com/microservice/ratingsdataservice/resource/RatingsResource.java
@@ -0,0 +1,4 @@
+package com.microservice.ratingsdataservice.resource;
+
+public class RatingsResource {
+}
